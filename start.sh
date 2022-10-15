b63366a782ee7845fc7caa3d6092134d930e6641
colin <xi-jian@outlook.com>
2022-10-15 13:57:55 +0800

    upgrade vsts-agent to 2.182.2

--- start.sh
+++ start.sh
@@ -22,23 +22,20 @@ if [ -n "$AZP_WORK" ]; then
   mkdir -p "$AZP_WORK"
 fi
 
-# rm -rf /azp/agent
-# if not exists then create it.
-if [ ! -x "/azp/agent/"]; then  
-　　mkdir /azp/agent
-fi  
-
-cd /azp/agent
-
 export AGENT_ALLOW_RUNASROOT="1"
 
 cleanup() {
   if [ -e config.sh ]; then
     print_header "Cleanup. Removing Azure Pipelines agent..."
 
-    ./config.sh remove --unattended \
-      --auth PAT \
-      --token $(cat "$AZP_TOKEN_FILE")
+    # If the agent has some running jobs, the configuration removal process will fail.
+    # So, give it some time to finish the job.
+    while true; do
+      ./config.sh remove --unattended --auth PAT --token $(cat "$AZP_TOKEN_FILE") && break
+
+      echo "Retrying in 30 seconds..."
+      sleep 30
+    done
   fi
 }
 
@@ -48,27 +45,31 @@ print_header() {
   echo -e "${lightcyan}$1${nocolor}"
 }
 
+# rm -rf /azp/agent
+# if not exists then create it.
+if [ ! -x "/azp/agent/"]; then  
+  mkdir /azp/agent
+fi  
+
+cd /azp/agent
+
 # Let the agent ignore the token env variables
 export VSO_AGENT_IGNORE=AZP_TOKEN,AZP_TOKEN_FILE
 
 print_header "1. Determining matching Azure Pipelines agent..."
 
-# AZP_AGENTPACKAGE_FILE=$(echo 'vsts-agent-linux-x64-2.154.3.tar.gz')
-AZP_AGENTPACKAGE_FILE=$(echo '{your_agent_file}')
+# AZP_AGENTPACKAGE_FILE=$(echo 'vsts-agent-linux-x64-2.181.2.tar.gz')
+AZP_AGENTPACKAGE_FILE=$(echo 'vsts-agent-linux-x64-2.181.2.tar.gz')
 
 if [ -o "$AZP_AGENTPACKAGE_FILE" == "null" ]; then
-  echo 1>&2 "error: could not determine a matching Azure Pipelines agent - check that account '$AZP_URL' is correct and the token is valid for that account"
+  echo 1>&2 "error: could not determine a matching Azure Pipelines agent - check that file '$AZP_AGENTPACKAGE_FILE' is exists."
   exit 1
 fi
 
 print_header "2. Installing Azure Pipelines agent..."
- 
 tar zxvf $AZP_AGENTPACKAGE_FILE
 
 source ./env.sh
-
-trap 'cleanup; exit 130' INT
-trap 'cleanup; exit 143' TERM
 
 print_header "3. Configuring Azure Pipelines agent..."
 
@@ -84,6 +85,12 @@ print_header "3. Configuring Azure Pipelines agent..."
 
 print_header "4. Running Azure Pipelines agent..."
 
-# `exec` the node runtime so it's aware of TERM and INT signals
-# AgentService.js understands how to handle agent self-update and restart
-exec ./externals/node10/bin/node ./bin/AgentService.js interactive
+trap 'cleanup; exit 0' EXIT
+trap 'cleanup; exit 130' INT
+trap 'cleanup; exit 143' TERM
+
+chmod +x ./run.sh
+
+# To be aware of TERM and INT signals call run.sh
+# Running it with the --once flag at the end will shut down the agent after the build is executed
+./run.sh "$@" & wait $!
